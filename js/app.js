9b34f77937bf7bcb5a6e20b01a1c2c7bb402e5a0
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-07 15:28:51 +0100

    FEAT thinking about the problem

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 //Creare un prompt con while che chiede numeri all'utente,
 //PERO' fare si che si fermi dopo aver ricevuto 10 numeri dispari
 
-let number = parseInt (prompt('inserisci un numero dispari'))
+//dichiaro variabili necessarie, ovvero chiedi numero, dispari e pari
+
+let = number
 
 const numberOdd = number % 2 >= 1 //il numero e' dispari
 
+const sogliaOdd = numberOdd
+
 const numberEven = number % 2 === 0 // il numero e' pari
 
-while (number = numberOdd) {
-  console.log(number)
+while (numberOdd < sogliaOdd) {
+  number = parseInt(prompt('inserisci 10 numeri dispari'))
 }
